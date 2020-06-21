d2ca2e48a179b997de0593c66594e2bfad3ecb82
Hennan Gadelha <hennanhennan@gmail.com>
2020-06-21 20:05:15 -0300

    criando modulo e componente da calculadora

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit} from '@angular/core'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './componentes/';
+
+
+
+@NgModule({
+  declarations: [CalculadoraComponent],
+  imports: [
+    CommonModule,
+  ],
+  exports:[CalculadoraComponent]
+})
+export class CalculadoraModule  {
+
+  
+
+ }
